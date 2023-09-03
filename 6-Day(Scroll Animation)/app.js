544db9b05f0bfb5cdf35a816dad738420acd00b9
Shivam Kumar <119694506+kumarshivam04203@users.noreply.github.com>
2023-09-03 12:50:48 +0530

    Add files via upload

---
+++ 6-Day(Scroll Animation)/app.js
@@ -0,0 +1,22 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes(){
+    const triggerButton = window.innerHeight / 5 * 4;
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+        if(boxTop < triggerButton){
+            box.classList.add('show');
+        }else{
+            box.classList.remove('show');
+        }
+    })
+}
+
+
+
+// The "Element.getBoundingClientRect()"" method returns a "DOMRect" object providing information about the size of an element and its position relative to the "viewport".
